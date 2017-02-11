034396d920e339b285951a38e9a57bef4fb248c4
Lamis Beirkdar <lamis.beirkdar@gmail.com>
2017-02-11 14:52:34 +0100

    Fix mistakes

--- public/index.php
+++ public/index.php
@@ -8,12 +8,12 @@ $app = new Silex\Application();
 if(!in_array(trim($env),['production', 'development'])) {
     Throw new \ErrorException("Something wrong happened, cannot proceed");
 }
+$env = file_get_contents(dirname(__DIR__). '/.env');
 
 require dirname(__DIR__) . "/config/$env/config.php";
 require dirname(__DIR__) . '/config/register.php';
 require dirname(__DIR__) .'/config/services.php';
 require dirname(__DIR__) .'/config/routes.php';
-
 
 $app->run();
 
